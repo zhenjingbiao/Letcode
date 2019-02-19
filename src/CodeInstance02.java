f2839bd4a9d5cf0a2fc24394f52c2a484df1a0eb
jingbiaozhen <jingbiao.zhen@ximalaya.com>
2019-02-19 16:47:08 +0800

    Initial commit

---
+++ src/CodeInstance02.java
@@ -0,0 +1,30 @@
+//题目：请实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy
+public class CodeInstance02 {
+    public static void main(String[] args) {
+
+        String str="We Are Happy haha";
+        System.out.println(replaceStr(new StringBuffer(str)));
+        System.out.println(replaceStr2(new StringBuffer(str)));
+
+    }
+    public static String replaceStr(StringBuffer sb){
+        if(sb!=null){
+           String []strArray=sb.toString().split(" ");
+           StringBuilder stringBuilder=new StringBuilder();
+            for (int i = 0; i < strArray.length; i++) {
+                if(i==strArray.length-1){
+                    continue;
+                }
+                stringBuilder.append(strArray[i]);
+                stringBuilder.append("%20");
+            }
+            stringBuilder.append(strArray[strArray.length-1]);
+            return stringBuilder.toString();
+        }
+        return "";
+    }
+    public static String replaceStr2(StringBuffer stringBuffer){//\s表示空格，\\s进行转义
+        return stringBuffer.toString().replaceAll("\\s","%20");
+    }
+
+}
---
+++ src/Test.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+       /* Map<String, String> map = new HashMap<String, String>();
+        map.put("apple", "苹果");
+        map.put("watermelon", "西瓜");
+        map.put("banana", "香蕉");
+        map.put("peach", "桃子");
+
+        Iterator iter = map.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }*/
+        CopyOnWriteArrayList<String>copyOnWriteArrayList=new CopyOnWriteArrayList<>();
+        copyOnWriteArrayList.add("苹果");
+        copyOnWriteArrayList.add("西瓜");
+        copyOnWriteArrayList.add("香蕉");
+        copyOnWriteArrayList.add("桃子");
+        for (String s : copyOnWriteArrayList) {
+            System.out.println(s);
+            copyOnWriteArrayList.remove("香蕉");
+        }
+    }
+
+}
