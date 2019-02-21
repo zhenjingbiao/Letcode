5a3c80e28a09e155d73b9c7bbc6a94d3735510bc
jingbiaozhen <jingbiao.zhen@ximalaya.com>
2019-02-21 21:56:27 +0800

    code everyday

---
+++ src/CodeInstance09.java
@@ -0,0 +1,12 @@
+//题目：
+//一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+
+public class CodeInstance09 {
+
+    public static void main(String[] args) {
+        int i = 3;
+        System.out.println(1<<--i);
+    }
+
+
+}
